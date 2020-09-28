39ac59e115ccea637c6a2ed83a3f86f5c4dffb0e
mike-ske <66280654+mike-ske@users.noreply.github.com>
2020-09-28 18:06:13 +0100

    new commits

--- admin/forget_pass.php
+++ admin/forget_pass.php
@@ -77,8 +77,8 @@ try {
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'alumonamicah@gmail.com';                     // SMTP username
     $mail->Password   = 'alumona123';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom('micahalumona@gmail.com', 'Readit Blog');
